7629a53077c945746695275d922da5db9e6b01c3
kim2103 <thpdp.lenguyenthienkim@gmail.com>
2023-10-21 13:44:29 +0700

    upload search

--- pages/script.js
+++ pages/script.js
@@ -10,6 +10,7 @@ import {
   query,
   where,
   limit,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/10.2.0/firebase-firestore.js";
 
 import {
@@ -34,24 +35,6 @@ const db = getFirestore(app);
 const auth = getAuth(app);
 
 let user = null;
-let cart = [];
-
-// async function getCart() {
-//   const q = query(
-//     collection(db, "carts"),
-//     where("userId", "==", user.uid),
-//     limit(1)
-//   );
-//   const querySnapshot = await getDocs(q);
-//   console.log("querySnapshot: ", querySnapshot);
-//   querySnapshot.forEach((doc) => {
-//     console.log(doc.id, " => ", doc.data());
-//     cart.push(doc.id);
-//   });
-
-//   if (!cart.length) {
-//   }
-// }
 
 async function getData() {
   const querySnapshot = await getDocs(collection(db, "books"));
@@ -66,13 +49,11 @@ async function getData() {
     imgContainer.href = `description.html?id=` + item.id;
     imgContainer.classList.add("image-container");
 
-    // Tạo img và đặt vào bên trong img container
     let image = document.createElement("img");
     image.setAttribute("src", item.data().imageUrl);
-    imgContainer.appendChild(image); // Đặt img vào img container
-    card.appendChild(imgContainer); // Đặt img container vào card
+    imgContainer.appendChild(image);
+    card.appendChild(imgContainer);
 
-    // Tạo div chứa thông tin sản phẩm: text container
     let container = document.createElement("div");
     container.classList.add("container");
 
@@ -84,7 +65,7 @@ async function getData() {
 
     // Giá tiền
     let price = document.createElement("h6");
-    price.innerHTML = "<b>Giá:</b> " + item.data().price; // In đậm chữ price nên ta phải dùng <b>
+    price.innerHTML = "<b>Giá:</b> " + item.data().price;
     container.appendChild(price);
 
     // thêm nút vào card
@@ -128,4 +109,67 @@ onAuthStateChanged(auth, (cUser) => {
     logOutBtn.classList.add("d-none");
     spanContainer.innerHTML = `<a href="./login.html" class="nav-bar-link">Tài khoản của tôi</a>`;
   }
+});
+
+const search = document.getElementById("search-field");
+search.addEventListener("change", async () => {
+  const value = search.value;
+  const docRef = doc(db, "books", item.id);
+  const docSnap = await getDoc(docRef);
+  fetch(docSnap.data())
+    .then((reponse) => reponse.json())
+    .then((data) => {
+      const products = data;
+      document.getElementById("products").innerHTML = "";
+      for (let items of products.data)
+        if (items.name.toUpperCase().includes(value.toUpperCase()) == true) {
+          // Tạo div card
+          let card = document.createElement("div");
+          card.classList.add("card");
+
+          // Tạo div chứa img: img container
+          let imgContainer = document.createElement("div");
+          imgContainer.classList.add("image-container");
+
+          // Tạo img và đặt vào bên trong img container
+          let image = document.createElement("img");
+          image.setAttribute("src", item.data.imageUrl);
+          imgContainer.appendChild(image); // Đặt img vào img container
+          card.appendChild(imgContainer); // Đặt img container vào card
+
+          // Tạo div chứa thông tin sản phẩm: text container
+          let container = document.createElement("div");
+          container.classList.add("container");
+
+          // Tên sản phẩm
+          let name = document.createElement("h5");
+          name.classList.add("product-name");
+          name.innerText = item.data().title.toUpperCase();
+          container.appendChild(name);
+
+          // Giá tiền
+          let price = document.createElement("h6");
+          price.innerHTML = "<b>Price:</b> " + item.data().price; // In đậm chữ price nên ta phải dùng <b>
+          container.appendChild(price);
+          // Thêm card sản phẩm vào danh sách products
+          card.appendChild(container);
+          document.getElementById("products").appendChild(card);
+        }
+
+      for (let items of products.data) {
+        let product = [];
+        if (localStorage.product) {
+          product = JSON.parse(localStorage.product);
+        }
+        product.push({
+          name: item.data().title,
+          price: item.data().price,
+          image: items.data().imageUrl,
+        });
+        localStorage.setItem("product", JSON.stringify(product));
+      }
+    })
+    .catch((error) => {
+      alert("Lỗi!");
+    });
 });
